33e2d7cdb3bb528a96ab69ad64653cf47c85decd
0xShaka <0xShaka@gmail.com>
2024-01-21 07:20:32 +0400

    Made one switch-case

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -31,7 +31,7 @@ export class GildedRose {
         this.increaseQuality(item)
         break
       case 'Backstage passes to a TAFKAL80ETC concert':
-        this.increaseTicketQuality(item)
+        this.updateTicketQuality(item)
         break
       case 'Sulfuras, Hand of Ragnaros':
         break
@@ -40,18 +40,6 @@ export class GildedRose {
     }
 
     this.updateSellInDays(item)
-    
-    if (item.sellIn >= 0) return
-
-    switch (item.name) {
-      case 'Backstage passes to a TAFKAL80ETC concert':
-        // Ticket expiry
-        item.quality = 0
-        break
-      default:
-        this.decreaseQuality(item)
-        break
-    }
   }
 
   private updateSellInDays(item: Item) {
@@ -65,14 +53,16 @@ export class GildedRose {
     if (item.quality < 50) item.quality += 1
   }
 
-  private decreaseQuality(item: Item) {
-    if (item.name === 'Aged Brie') return
-    if (item.name === 'Sulfuras, Hand of Ragnaros') return
-    
+  private decreaseQuality(item: Item) {    
     if (item.quality > 0) item.quality -= 1
+    if (item.sellIn <= 0 && item.quality > 0) item.quality -= 1
   }
 
-  private increaseTicketQuality(item: Item) {
+  private updateTicketQuality(item: Item) {
+    if (item.sellIn <= 0) { 
+      item.quality = 0 
+      return 
+    }
     if (item.quality >= 50) return 
 
     item.quality = item.quality + 1
@@ -84,5 +74,6 @@ export class GildedRose {
     if (item.sellIn <= 5 && item.quality < 50) {
       item.quality = item.quality + 1
     }
+
   }
 }
