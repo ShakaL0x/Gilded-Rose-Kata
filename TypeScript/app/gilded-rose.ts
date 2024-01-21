22962b7813c6cde361915226ecaa04b5f34496dc
0xShaka <0xShaka@gmail.com>
2024-01-21 04:44:08 +0400

    Made first if/else clear

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -24,22 +24,27 @@ export class GildedRose {
   }
 
   updateItem(item: Item) {
-    if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (item.quality > 0) {
-        if (item.name != 'Sulfuras, Hand of Ragnaros') {
-          item.quality = item.quality - 1
-        }
-      }
+    if (
+      item.quality > 0 &&
+      item.name != 'Aged Brie' && 
+      item.name != 'Backstage passes to a TAFKAL80ETC concert' &&
+      item.name != 'Sulfuras, Hand of Ragnaros'
+    ) {
+      item.quality = item.quality - 1
     } else {
+      // Increase +1 or ticket handling
       if (item.quality < 50) {
         item.quality = item.quality + 1
+        
         if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (item.sellIn < 11) {
+          
+          if (item.sellIn <= 10) {
             if (item.quality < 50) {
               item.quality = item.quality + 1
             }
           }
-          if (item.sellIn < 6) {
+          
+          if (item.sellIn <= 5) {
             if (item.quality < 50) {
               item.quality = item.quality + 1
             }
@@ -47,9 +52,12 @@ export class GildedRose {
         }
       }
     }
+
+
     if (item.name != 'Sulfuras, Hand of Ragnaros') {
       item.sellIn = item.sellIn - 1;
     }
+
     if (item.sellIn < 0) {
       if (item.name != 'Aged Brie') {
         if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
